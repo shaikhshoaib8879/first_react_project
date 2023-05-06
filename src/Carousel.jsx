e616cc6d01b802a453a3e42fcb4427bd084ef1d4
shoaib shaikh <shoaib.shaikh@mirraw.com>
2023-05-06 11:37:05 +0530

    first commit

---
+++ src/Carousel.jsx
@@ -0,0 +1,46 @@
+// class component 
+import { Component } from "react";
+
+class Carousel extends Component {
+  // debugger
+  state = {
+    active: 0,
+  };
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+  handleIndexClick = (event) => {
+    // debugger
+    console.log(event)
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
+  render() {
+    // debugger
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
+// Footer
---
+++ src/Pet.jsx
@@ -0,0 +1,40 @@
+// const Pet = (props) => {
+//     return (
+//       <div>
+//         <h1>{props.name}</h1>
+//         <h2>{props.animal}</h2>
+//         <h2>{props.breed}</h2>
+//         <h2>{props.id}</h2>
+//         <img src={props.images[0]}/>
+
+//       </div>
+//     );
+//   };
+  
+//   export default Pet;
+
+import { Link } from "react-router-dom";
+
+
+const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
+
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
+  return (
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </Link>
+  );
+};
+
+export default Pet;
